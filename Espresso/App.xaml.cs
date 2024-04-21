1e10db46df2e14fc2c506a0dc629c4a1b57033d7
thejackbitt <jack.bittner@outlook.com>
2024-04-21 17:20:47 -0500

    baseline MAUI content

---
+++ Espresso/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Espresso
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
