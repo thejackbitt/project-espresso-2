9f58fd79dbe9613d39b04cacdb77e4423e1e4989
thejackbitt <jack.bittner@outlook.com>
2024-05-08 21:32:32 -0500

    working hamburger menu

--- Espresso/CustomNavigationBar.xaml.cs
+++ Espresso/CustomNavigationBar.xaml.cs
@@ -1,9 +1,33 @@
-namespace Espresso;
+using System.ComponentModel;
+using System.Windows.Input;
+using Microsoft.Maui.Controls;
 
-public partial class CustomNavigationBar : ContentView
+namespace Espresso
 {
-	public CustomNavigationBar()
-	{
-		InitializeComponent();
-	}
+    public partial class CustomNavigationBar : ContentView, INotifyPropertyChanged
+    {
+        private bool _isMenuVisible;
+        public bool IsMenuVisible
+        {
+            get => _isMenuVisible;
+            set
+            {
+                _isMenuVisible = value;
+                OnPropertyChanged(nameof(IsMenuVisible));
+            }
+        }
+
+        public ICommand ToggleMenuCommand { get; }
+
+        public CustomNavigationBar()
+        {
+            InitializeComponent();
+            ToggleMenuCommand = new Command(() => IsMenuVisible = !IsMenuVisible);
+            BindingContext = this;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 }
--- Espresso/obj/Debug/net8.0-android/Espresso.AssemblyInfo.cs
+++ Espresso/obj/Debug/net8.0-android/Espresso.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Espresso")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+36c8283dd56b037fe6e89ca276a4a29199856866")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b1cca2b446368a2c896bc4205a9f13350af0aa7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Espresso")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Espresso")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
